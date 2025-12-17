37db9e1256aa859caf1a3610f35e496f5cf497ef
wangziyan <zwang53@charlotte.edu>
2025-12-16 22:18:21 -0500

    bug fix for not logging metrics change

--- train_trans_new.py
+++ train_trans_new.py
@@ -24,7 +24,6 @@ warnings.filterwarnings('ignore')
 os.environ['TOKENIZERS_PARALLELISM'] = 'false'
 device = torch.device('cuda')
 
-
 ##### ---- Exp dirs ---- #####
 args = option_trans.get_args_parser()
 torch.manual_seed(args.seed)
@@ -54,6 +53,7 @@ eval_wrapper = EvaluatorModelWrapper(wrapper_opt)
 
 ##### ---- GloVe ---- #####
 from utils.word_vectorizer import WordVectorizer
+
 w_vectorizer = WordVectorizer('./glove', 'our_vab')
 
 ##### ---- ModernBERT ---- #####
@@ -104,7 +104,8 @@ scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=args.lr_s
 codebooks = {'train': codebook_train_dir, 'val': codebook_val_dir, 'test': codebook_test_dir}
 for type, codebook_dir in codebooks.items():
     if len(os.listdir(codebook_dir)) == 0:
-        dataloader_token = dataset_tokenize.DATALoader(args.dataname, type=type, batch_size=1, unit_length=2 ** args.down_t)
+        dataloader_token = dataset_tokenize.DATALoader(args.dataname, type=type, batch_size=1,
+                                                       unit_length=2 ** args.down_t)
         for batch in dataloader_token:
             pose, name = batch
             pose = pose.to(device).float()  # bs, nb_joints, joints_dim, seq_len
@@ -113,8 +114,10 @@ for type, codebook_dir in codebooks.items():
             np.save(os.path.join(codebook_dir, f'{name[0]}.npy'), target)
 
 ##### ---- Dataloader ---- #####
-train_loader = dataset_TM_train.DATALoader(args.dataname, codebook_train_dir, args.nb_code, args.batch_size, unit_length=2**args.down_t)
+train_loader = dataset_TM_train.DATALoader(args.dataname, codebook_train_dir, args.nb_code, args.batch_size,
+                                           unit_length=2 ** args.down_t)
 train_loader_iter = dataset_TM_train.cycle(train_loader)
+
 
 ##### ---- Training ---- #####
 def get_acc(cls_pred, target, mask):
@@ -124,6 +127,7 @@ def get_acc(cls_pred, target, mask):
     _, cls_pred_index = torch.max(probs, dim=-1)
     right_num = (cls_pred_index == target_all).sum()
     return right_num * 100 / mask.sum()
+
 
 def masking(ids, seq_lens: torch.Tensor, batch_size, max_len, mask_id, probs: list):
     if args.pkeep == -1:
@@ -157,21 +161,25 @@ def masking(ids, seq_lens: torch.Tensor, batch_size, max_len, mask_id, probs: li
         seq_mask = generate_src_mask(max_len, seq_lens + 1)
         return masked_input_indices, seq_mask_no_end, seq_mask, mask_token
 
+
 def construct_pred_and_label(pred, ids, seq_mask_no_end):
-    weights = seq_mask_no_end / (seq_mask_no_end.sum(-1).unsqueeze(-1) * seq_mask_no_end.shape[0])  # weights[i, j] = 1 / (num_valid * B)
+    weights = seq_mask_no_end / (
+                seq_mask_no_end.sum(-1).unsqueeze(-1) * seq_mask_no_end.shape[0])  # weights[i, j] = 1 / (num_valid * B)
     pred_seq_masked = pred[seq_mask_no_end, :].view(-1, pred.shape[-1])  # (num_valid, vocab)
-    target_seq_masked = ids[seq_mask_no_end]                             # (num_valid,)
-    weight_seq_masked = weights[seq_mask_no_end]                         # (num_valid,)
+    target_seq_masked = ids[seq_mask_no_end]  # (num_valid,)
+    weight_seq_masked = weights[seq_mask_no_end]  # (num_valid,)
 
     return pred_seq_masked, target_seq_masked, weight_seq_masked
 
+
 def compute_result(pred_seq_masked, target, seq_mask_no_end):
-    probs_seq_masked = torch.softmax(pred_seq_masked, dim=-1)         # (num_valid, vocab)
-    _, pred_seq_masked_index = torch.max(probs_seq_masked, dim=-1)    # (num_valid,)
+    probs_seq_masked = torch.softmax(pred_seq_masked, dim=-1)  # (num_valid, vocab)
+    _, pred_seq_masked_index = torch.max(probs_seq_masked, dim=-1)  # (num_valid,)
     target_seq_masked = torch.masked_select(target, seq_mask_no_end)  # (num_valid,)
     right_seq_masked = (pred_seq_masked_index == target_seq_masked).sum()  # compare with label
 
     return right_seq_masked
+
 
 def train(first_modality, mask_probs):
     # Get masking probabilities
@@ -186,6 +194,21 @@ def train(first_modality, mask_probs):
     }
     invalid_ids_t = [special_ids_t['eos_id'], special_ids_t['pad_id']]
 
+    ##### ---- Training ---- #####
+    best_fid = 1000
+    best_iter_m = 0
+    best_div = 100
+    best_top1 = 0
+    best_top2 = 0
+    best_top3 = 0
+    best_matching = 100
+    best_iter_t = 0
+    best_bleu1 = 0.
+    best_bleu2 = 0.
+    best_bleu3 = 0.
+    best_bleu4 = 0.
+    best_rouge_l = 0.
+
     for nb_iter in tqdm(range(1, args.total_iter + 1), position=0, leave=True):
         batch = next(train_loader_iter)
         text, token_ids_m, lens_m = batch
@@ -197,7 +220,7 @@ def train(first_modality, mask_probs):
 
         # Encode all texts into text tokens for training
         text_inputs = tokenizer(text, padding='max_length', truncation=True, max_length=args.max_t, return_tensors='pt')
-        token_ids_t = text_inputs['input_ids'].to(device)      # (bs, max_t)
+        token_ids_t = text_inputs['input_ids'].to(device)  # (bs, max_t)
         seq_mask_t = text_inputs['attention_mask'].to(device)  # (bs, max_t)
 
         # Get lengths for each text in batch
@@ -205,8 +228,10 @@ def train(first_modality, mask_probs):
         lens_t = valid_mask_t.sum(dim=1)  # (bs,)
 
         # Mask
-        masked_input_ids_m, seq_mask_no_end_m, seq_mask_m, mask_token_m = masking(token_ids_m, lens_m, bs, max_m, mask_id_m, probs_m)
-        masked_input_ids_t, seq_mask_no_end_t, mask_token_t = masking(token_ids_t, lens_t, bs, args.max_t, mask_id_t, probs_t)
+        masked_input_ids_m, seq_mask_no_end_m, seq_mask_m, mask_token_m = masking(token_ids_m, lens_m, bs, max_m,
+                                                                                  mask_id_m, probs_m)
+        masked_input_ids_t, seq_mask_no_end_t, mask_token_t = masking(token_ids_t, lens_t, bs, args.max_t, mask_id_t,
+                                                                      probs_t)
 
         # Combine motion mask and text mask
         if first_modality == 'motion':
@@ -222,15 +247,18 @@ def train(first_modality, mask_probs):
         # Train trans: forward
         pred_m, pred_t = trans_encoder(masked_input_ids_m,
                                        src_mask=src_mask,
-                                       att_txt=torch.empty(bs, 0, dtype=torch.bool, device=device),  # empty mask for CLS token
+                                       att_txt=torch.empty(bs, 0, dtype=torch.bool, device=device),
+                                       # empty mask for CLS token
                                        word_emb=emb_t,
                                        first=first_modality,
                                        max_m=max_m,
                                        max_t=args.max_t)  # (bs, max_m, vocab), (bs, max_t, vocab)
 
         # Compute loss as a batch
-        pred_seq_masked_m, target_seq_masked_m, weight_seq_masked_m = construct_pred_and_label(pred_m, token_ids_m, seq_mask_no_end_m)
-        pred_seq_masked_t, target_seq_masked_t, weight_seq_masked_t = construct_pred_and_label(pred_t, token_ids_t, seq_mask_no_end_t)
+        pred_seq_masked_m, target_seq_masked_m, weight_seq_masked_m = construct_pred_and_label(pred_m, token_ids_m,
+                                                                                               seq_mask_no_end_m)
+        pred_seq_masked_t, target_seq_masked_t, weight_seq_masked_t = construct_pred_and_label(pred_t, token_ids_t,
+                                                                                               seq_mask_no_end_t)
 
         loss_m = F.cross_entropy(pred_seq_masked_m, target_seq_masked_m, reduction='none')
         loss_m = (loss_m * weight_seq_masked_m).sum()
@@ -269,21 +297,27 @@ def train(first_modality, mask_probs):
                 num_repeat = -30
                 rand_pos = True
                 val_loader = dataset_TM_eval.DATALoaderNew(args.dataname, codebook_test_dir, w_vectorizer, args.nb_code,
-                                                           batch_size=32, is_test=True, tokenizer_t=tokenizer, max_t=args.max_t)
+                                                           batch_size=32, is_test=True, tokenizer_t=tokenizer,
+                                                           max_t=args.max_t)
             else:
                 num_repeat = 1
                 rand_pos = False
                 val_loader = dataset_TM_eval.DATALoaderNew(args.dataname, codebook_val_dir, w_vectorizer, args.nb_code,
-                                                           batch_size=32, is_test=False, tokenizer_t=tokenizer, max_t=args.max_t)
+                                                           batch_size=32, is_test=False, tokenizer_t=tokenizer,
+                                                           max_t=args.max_t)
 
             best_iter_m, best_fid, best_div, best_top1, best_top2, best_top3, best_matching, best_multi = eval_trans_m(
                 args.out_dir, val_loader, net, trans_encoder, logger, writer, nb_iter,
-                eval_wrapper, max_m, args.max_t, first_modality,
+                eval_wrapper, max_m, args.max_t, first_modality, best_iter=best_iter_m, best_fid=best_fid,
+                best_div=best_div, best_top1=best_top1, best_top2=best_top2, best_top3=best_top3,
+                best_matching=best_matching,
                 num_repeat=num_repeat, rand_pos=rand_pos)
 
             best_iter_t, best_bleu1, best_bleu2, best_bleu3, best_bleu4, best_rouge_l = eval_trans_t(
                 args.out_dir, val_loader, net, trans_encoder, logger, writer, nb_iter,
                 eval_wrapper, tokenizer, special_ids_t, invalid_ids_t, max_m, args.max_t, first_modality,
+                best_iter=best_iter_t, best_bleu1=best_bleu1, best_bleu2=best_bleu2, best_bleu3=best_bleu3,
+                best_bleu4=best_bleu4, best_rouge_l=best_rouge_l,
                 num_repeat=num_repeat, rand_pos=rand_pos)
 
             if nb_iter == args.total_iter:
@@ -296,8 +330,10 @@ def train(first_modality, mask_probs):
                 logger.info(msg_final)
                 break
 
+
 # Training Step 1: mix training
 bests = train(
-    first_modality='motion',               # "motion" first or "text" first
-    mask_probs=((0.0001, 1), (0.0001, 1))  # ((prob_lower_bound_m, prob_upper_bound_m), (prob_lower_bound_t, prob_upper_bound_t))
+    first_modality='motion',  # "motion" first or "text" first
+    mask_probs=((0.0001, 1), (0.0001, 1))
+    # ((prob_lower_bound_m, prob_upper_bound_m), (prob_lower_bound_t, prob_upper_bound_t))
 )
